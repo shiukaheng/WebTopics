6bc7ba27e9f3ea832f57fd68c71518658fa7a842
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-02-07 01:09:53 +0000

    added props to channel object

--- src/utils/Channel.ts
+++ src/utils/Channel.ts
@@ -1,7 +1,23 @@
 import { z } from "zod";
 import { JSONValue } from "./JSON";
 
+export const channelModeSchema = z.union([
+    z.literal("state"),
+    z.literal("command"),
+]);
+
+export type ChannelMode = z.infer<typeof channelModeSchema>;
+
 export type Channel<T extends JSONValue> = {
     name: string;
+    mode: ChannelMode;
     schema: z.ZodSchema<T>;
+    responseSchema?: z.ZodSchema<T>;
 }
+
+export const channelSchema = z.object({
+    name: z.string(),
+    mode: channelModeSchema,
+    schema: z.unknown(),
+    responseSchema: z.unknown().optional(),
+});
