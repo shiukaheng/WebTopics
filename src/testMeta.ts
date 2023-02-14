68ba2361508cbc6534105bba375bde49e74fb611
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-02-14 16:20:33 +0000

    changed package.json

--- src/testMeta.ts
+++ src/testMeta.ts
@@ -1,40 +1,40 @@
-import { z } from "zod";
-import { Server } from "socket.io";
-import { TopicServer } from "./Server";
-import { TopicClient } from "./Client";
-import { createService, createTopic } from "./utils/createChannel";
-import { serverMetaChannel } from "./metaChannels";
-import { io } from "socket.io-client";
+// import { z } from "zod";
+// import { Server } from "socket.io";
+// import { TopicServer } from "./Server";
+// import { TopicClient } from "./Client";
+// import { createService, createTopic } from "./utils/createChannel";
+// import { serverMetaChannel } from "./metaChannels";
+// import { io } from "socket.io-client";
 
-// Create a server
-const server = new TopicServer(new Server(3002));
+// // Create a server
+// const server = new TopicServer(new Server(3002));
 
-// Create a test service
-const testService = createService("testService",
-    z.object({
-        test: z.string(),
-    }),
-    z.object({
-        test: z.string(),
-    })
-);
+// // Create a test service
+// const testService = createService("testService",
+//     z.object({
+//         test: z.string(),
+//     }),
+//     z.object({
+//         test: z.string(),
+//     })
+// );
 
 
-server.sub(serverMetaChannel, (data)=>{
-    console.log("🎊 Server meta:", data);
-})
+// server.sub(serverMetaChannel, (data)=>{
+//     console.log("🎊 Server meta:", data);
+// })
 
-// Make the 3 clients connect 1 by 1 with a 1 second delay
-setTimeout(()=>{
-    const client1 = new TopicClient(io("http://localhost:3002"));
-    client1.sub(serverMetaChannel, (data)=>{
-        console.log("🎊 Client 1 server meta:", data);
-    })
-    client1.srv(testService, (data)=>{
-        console.log("🎊 Client 1 test service:", data);
-        return {
-            test: "Client 1 response"
-        }
-    })
-}
-, 1000);
+// // Make the 3 clients connect 1 by 1 with a 1 second delay
+// setTimeout(()=>{
+//     const client1 = new TopicClient(io("http://localhost:3002"));
+//     client1.sub(serverMetaChannel, (data)=>{
+//         console.log("🎊 Client 1 server meta:", data);
+//     })
+//     client1.srv(testService, (data)=>{
+//         console.log("🎊 Client 1 test service:", data);
+//         return {
+//             test: "Client 1 response"
+//         }
+//     })
+// }
+// , 1000);
--- src/testService.ts
+++ src/testService.ts
@@ -1,47 +1,47 @@
-import { z } from "zod";
-import { Server } from "socket.io";
-import { TopicServer } from "./Server";
-import { TopicClient } from "./Client";
-import { createService } from "./utils/createChannel";
+// import { z } from "zod";
+// import { Server } from "socket.io";
+// import { TopicServer } from "./Server";
+// import { TopicClient } from "./Client";
+// import { createService } from "./utils/createChannel";
 
-const testChannel = createService("test",
-    z.object({
-        a: z.number(),
-        b: z.number()
-    }),
-    z.object({
-        c: z.number()
-    })
-)
+// const testChannel = createService("test",
+//     z.object({
+//         a: z.number(),
+//         b: z.number()
+//     }),
+//     z.object({
+//         c: z.number()
+//     })
+// )
 
-const socketIOServer = new Server(3000);
-const topicServer = new TopicServer(socketIOServer);
-topicServer.srv(testChannel, ({a, b}) => {
-    return {
-        c: a + b
-    }
-});
+// const socketIOServer = new Server(3000);
+// const topicServer = new TopicServer(socketIOServer);
+// topicServer.srv(testChannel, ({a, b}) => {
+//     return {
+//         c: a + b
+//     }
+// });
 
-// Client 1
-const topicClient1 = new TopicClient("http://localhost:3000");
+// // Client 1
+// const topicClient1 = new TopicClient("http://localhost:3000");
 
-// Client 2
-const topicClient2 = new TopicClient("http://localhost:3000");
+// // Client 2
+// const topicClient2 = new TopicClient("http://localhost:3000");
 
-// Send updates regularly from client 1
-setInterval(() => {
-    topicClient1.req(testChannel, {a: 1, b: 2}, topicClient1.serverID as string).then((response) => {
-        console.log(`Client 1 received response: ${JSON.stringify(response)}`)
-    }).catch((error) => {
-        console.log(`Client 1 received error: ${error}`)
-    });    
-}, 1000);
+// // Send updates regularly from client 1
+// setInterval(() => {
+//     topicClient1.req(testChannel, {a: 1, b: 2}, topicClient1.serverID as string).then((response) => {
+//         console.log(`Client 1 received response: ${JSON.stringify(response)}`)
+//     }).catch((error) => {
+//         console.log(`Client 1 received error: ${error}`)
+//     });    
+// }, 1000);
 
-// Send updates regularly from client 1
-setInterval(() => {
-    topicClient2.req(testChannel, {a: 2, b: 3}, topicClient1.serverID as string).then((response) => {
-        console.log(`Client 2 received response: ${JSON.stringify(response)}`)
-    }).catch((error) => {
-        console.log(`Client 2 received error: ${error}`)
-    });    
-}, 1500);
+// // Send updates regularly from client 1
+// setInterval(() => {
+//     topicClient2.req(testChannel, {a: 2, b: 3}, topicClient1.serverID as string).then((response) => {
+//         console.log(`Client 2 received response: ${JSON.stringify(response)}`)
+//     }).catch((error) => {
+//         console.log(`Client 2 received error: ${error}`)
+//     });    
+// }, 1500);
