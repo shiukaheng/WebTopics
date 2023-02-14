018fbf356b89248634f4aa054abcacfb5489eb24
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-02-14 16:52:47 +0000

    fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { TopicServer } from "./Server";
 export { TopicClient } from "./Client";
+export { createTopic, createService } from "./utils/createChannel"
 export { Channel } from "./utils/Channel";
 export { JSONValue, JSONObject, JSONArray } from "./utils/JSON";
