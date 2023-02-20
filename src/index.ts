4ac97bc1569cc84751d08f5eac38beae6a3c36ca
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-02-20 00:29:08 +0000

    added meta channel to exports

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { TopicClient } from "./Client";
 export { createTopic, createService } from "./utils/createChannel"
 export { Channel } from "./utils/Channel";
 export { JSONValue, JSONObject, JSONArray } from "./utils/JSON";
+export { serverMetaSchema, serverMetaChannel, ServerMeta } from "./metaChannels";
