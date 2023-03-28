71ec44710666a01ab6be51405e36e4738e842e60
Heng <31088159+shiukaheng@users.noreply.github.com>
2023-03-28 21:30:37 +0100

    Update dev.ts

--- src/dev.ts
+++ src/dev.ts
@@ -2,22 +2,22 @@ import { TopicServer } from "./Server";
 import { TopicClient } from "./Client";
 import { Server } from "socket.io";
 import { io } from "socket.io-client";
+import { serverMetaChannel } from "./metaChannels";
 
 // Create a new TopicServer instance
 const ioServer = new Server(5100);
 const server = new TopicServer(ioServer);
 
-// Create a new TopicClient instance
+// Create a new TopicClient instance and disconnect in 1 second
 const socket = io("http://localhost:5100");
 const client = new TopicClient(socket);
 
-// Quickly call the client 10 times, 100ms apart, for client.getServerID() (it returns promise)
-for (let i = 0; i < 10; i++) {
-    setTimeout(() => {
-    client.getServerID(100).then((id) => {
-        // console.log(id);
-    }).catch((err) => {
-        console.log(`Error on ${i}`)
-    })
-    }, i * 3);
-}
+setTimeout(() => {
+    client.disconnect();
+    }, 2000);
+
+server.sub(serverMetaChannel, (data) => {
+    // console.log("Server received data:");
+    // console.log(data);
+    console.log("📡 Got:", data);
+});
